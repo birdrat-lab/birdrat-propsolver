745cb842f1619f090946d82f8ed0776952d1e759
birdrat <birdrat.jr@proton.me>
2026-05-15 21:01:31 -0400

    refactoring formula.lisp

--- src/kernel/formula.lisp
+++ src/kernel/formula.lisp
@@ -35,28 +35,40 @@ over a formula."
 		       implication-case
 		       malformed-case)))
     (t (funcall malformed-case formula))))
+     
 
 (defun formula-p (maybe-formula)
-  "For a given S expression, checks if it's a formula"
-  (let ((prop-var-case (lambda (f) t))
-	(negation-case (lambda (result) (and t result)))
-	(implication-case (lambda (left-result right-result) (and t left-result right-result)))
-	(malformed-case (lambda (f) nil)))
-    (formula-fold formula prop-var-case negation-case implication-case malformed-case)))
-
+  "For a passed object, checks if it's a formula"
+  (formula-fold maybe-formula
+		(lambda (f) (and (prop-var-name f) (symbolp (prop-var-name f))))
+		(lambda (result) (and t result))
+		(lambda (left-result right-result) (and t left-result right-result))
+		(lambda (f) nil)))
 
 (defun formula-depth (formula)
   "Calculates formula depth, i.e. how many operators are applied in one expression."
-  (let ((prop-var-case (lambda (a) 0))
-	(negation-case (lambda (result) (+ 1 result)))
-	(implication-case (lambda (left-result right-result) (+ 1 (max left-result right-result))))
-	(malformed-case (lambda (f)  (error "Malformed formula: ~S" f))))
-    (formula-fold formula prop-var-case negation-case implication-case malformed-case)))
-     
+  (formula-fold maybe-formula
+		(lambda (a) 0)
+		(lambda (result) (+ 1 result))
+		(lambda (left-result right-result) (+1 (max left-result right-result)))
+		(lambda (f)  (error "Malformed formula: ~S" f))))
+
+
+(defun formula-vars (formula)
+  "Count the number of variables in a formula."
+  (formula-fold maybe-formula
+		(lambda (a) 1)
+		(lambda (result) (result))
+		(lambda (left-result right-result) (+ left-result right-result))
+		(lambda (f)  (error "Malformed formula: ~S" f))))
+
+
 (defun formula-size (formula)
   "Calculates formula size, i.e. how many symbols there are in the whole expression."
-  (let ((prop-var-case (lambda (a) 1))
-	(negation-case (lambda (result) (+ 1 result)))
-	(implication-case (lambda (left-result right-result) (+ 1 left-result right-result)))
-	(malformed-case (lambda (f)  (error "Malformed formula: ~S" f))))
-    (formula-fold formula prop-var-case negation-case implication-case malformed-case)))
+  (formula-fold maybe-formula
+		(lambda (a) 1)
+		(lambda (result) (+ 1 result))
+		(lambda (left-result right-result) (+ 1 left-result right-result))
+		(lambda (f)  (error "Malformed formula: ~S" f))))
+
+
