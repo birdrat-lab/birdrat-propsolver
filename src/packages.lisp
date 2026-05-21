37bd46d4b75c39d9f3ed33e69a8e4bd020c6f428
birdrat <birdrat.jr@proton.me>
2026-05-21 14:31:36 -0400

    defining the kernel package

---
+++ src/packages.lisp
@@ -0,0 +1,30 @@
+(defpackage #:birdrat-propsolver/kernel
+  (:use #:cl)
+  (:export
+   ;; formula structs and generated constructors/accessors
+   #:prop-var
+   #:make-prop-var
+   #:prop-var-p
+   #:prop-var-name
+
+   #:negation
+   #:make-negation
+   #:negation-p
+   #:negation-formula
+
+   #:implication
+   #:make-implication
+   #:implication-p
+   #:implication-from
+   #:implication-to
+
+   ;; formula operations
+   #:formula-p
+   #:formula-depth
+   #:formula-size
+   #:formula-vars
+   #:formula=
+
+   ;; surface syntax conversion
+   #:sexp->formula
+   #:formula->sexp))
